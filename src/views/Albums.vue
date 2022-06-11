7c4932c56f774c91d18fbed5ab95e7d71948418d
benb <boumedyen121@gmail.com>
2022-06-11 21:02:02 +0100

    REF

--- src/views/Albums.vue
+++ src/views/Albums.vue
@@ -1,9 +1,9 @@
 <script setup>
-import Album from '@/components/Albums'
+import Albums from '@/components/Albums'
 </script>
 
 <template>
     <main>
-        <Album />
+        <Albums />
     </main>
 </template>
