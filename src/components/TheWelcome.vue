4f36af29eeafad9aa9a8e0a195edfb1158ff043d
benb <boumedyen121@gmail.com>
2022-06-11 17:30:36 +0100

    scaffolding

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+
+const data = ref(null)
+const error = ref(null)
+
+fetch('https://jsonplaceholder.typicode.com/albums')
+    .then((res) => res.json())
+    .then((json) => (data.value = json))
+    .catch((err) => (error.value = err))
+</script>
+
+<template>
+    <div v-if="error">Oops! Error encountered: {{ error.message }}</div>
+    <div v-else-if="data">
+        <ul>
+            <li v-for="album in data" >
+                {{album.title}}
+            </li>
+        </ul>
+
+    </div>
+    <div v-else>Loading...</div>
+</template>
