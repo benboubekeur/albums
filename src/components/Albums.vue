aa6210e4164fb3e7abf748705ef852f553f07144
benb <boumedyen121@gmail.com>
2022-06-12 20:25:18 +0100

    pagination

--- src/components/Albums.vue
+++ src/components/Albums.vue
@@ -1,19 +1,18 @@
 <script setup>
-import {onMounted, ref} from 'vue'
+import {onMounted, ref, watch} from 'vue'
 import Pagination from '@ocrv/vue-tailwind-pagination'
-
-
-let currentPage = ref(1)
-let perPage = ref(2)
-let total = ref(20)
 
 
 const data = ref(null)
 const error = ref(null)
 const ALBUMS_URL = 'https://jsonplaceholder.typicode.com/albums';
 
+let currentPage = ref(1)
+let perPage = ref(12)
+let total = ref(200)
 
-function fetchAlbums() {
+
+const fetchAlbums = () => {
     fetch(getRequestUrl())
         .then((res) => res.json())
         .then((json) => (data.value = json))
@@ -21,33 +20,40 @@ function fetchAlbums() {
 }
 
 const getRequestUrl = () => {
-    return `${ALBUMS_URL}?_page=${currentPage}&_limit=${perPage}`
+    return `${ALBUMS_URL}?_page=${currentPage.value}&_limit=${perPage.value}`
 }
 const pageChanged = (event) => {
-    currentPage = event
-    console.warn(`Page is ${event}`)
+    currentPage.value = event
 }
+watch(currentPage, (currentPage, prevCurrentPage) => {
+    fetchAlbums()
+})
 onMounted(() => {
-    fetchAlbums();
-
+    fetchAlbums()
 })
 </script>
 
 <template>
     <div v-if="error">Oops! Error encountered: {{ error.message }}</div>
 
-    <div v-else-if="data"
-         class="grid pt-4 grid-cols-1  sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
-        <div v-for="album in data" :key="album.id" class="">
-            <router-link :to="{ name: 'album', params: { id: album.id } }">
-                <div
-                    class="p-4 w-full text-center bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-100 mb-1 cursor-pointer">
-                    <h5 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">{{ album.title }}</h5>
-                </div>
-            </router-link>
+    <div v-else-if="data">
+        <div
+            class="grid pt-4 grid-cols-1  sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+            <div v-for="album in data" :key="album.id" class="">
+                <router-link :to="{ name: 'album', params: { id: album.id } }">
+                    <div
+                        class="p-4 w-full text-center bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-100 mb-1 cursor-pointer">
+                        <h5 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">{{ album.title }}</h5>
+                    </div>
+                </router-link>
+
+            </div>
 
         </div>
-        <Pagination :current="currentPage" :per-page="perPage" :total="total" @page-changed="pageChanged($event)"/>
+        <div class="grid pt-4 grid-cols-1  w-full">
+            <Pagination :current="currentPage" :per-page="perPage" :total="total"
+                        @page-changed="pageChanged($event)"/>
+        </div>
     </div>
     <div v-else>Loading...</div>
 
